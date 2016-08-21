cb99abafdc3e1b6c919bbb2cf4d07cd054d5b469
volunt22r <volunt22r@naver.com>
2016-08-21 14:48:21 +0900

    링크따오는 크롤러 유닛테스트 작성

--- src/main/java/study/java/project1/crawler/NewsLinkCrawler.java
+++ src/main/java/study/java/project1/crawler/NewsLinkCrawler.java
@@ -13,12 +13,13 @@ import study.java.project1.html.TagElement;
 import study.java.project1.model.CrawlRecipe;
 
 public class NewsLinkCrawler implements NewsCrawler<List<TagElement>> {
-
+  public static final int DEFAULT_TIMEOUT_MILLIS = 3000;
+  
   @Override
   public List<TagElement> parse(CrawlerContext context) throws Exception {
     CrawlRecipe recipe = context.getParam(CrawlerContextProperty.RECIPE);
     String seedUrl = context.getParam(CrawlerContextProperty.SEED_URL);
-    Document doc = Jsoup.parse(new URL(seedUrl), 3000);
+    Document doc = Jsoup.parse(new URL(seedUrl), DEFAULT_TIMEOUT_MILLIS);
     Elements elements = doc.select(recipe.getNewsLinkSelector());
     List<TagElement> links = new ArrayList<>();
     elements.forEach(element -> links.add(new AnchorTag(element)));
--- src/test/java/study/java/project1/crawler/CrawlerTest.java
+++ src/test/java/study/java/project1/crawler/CrawlerTest.java
@@ -4,6 +4,7 @@
 package study.java.project1.crawler;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
 import static org.powermock.api.mockito.PowerMockito.*;
 
 import java.io.File;
@@ -22,10 +23,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import study.java.project1.crawler.ContentsListExtractor.ContentsListProperty;
 import study.java.project1.crawler.NewsCrawler.CrawlerContext;
 import study.java.project1.crawler.NewsCrawler.CrawlerContextProperty;
 import study.java.project1.html.TagElement;
+import study.java.project1.model.CrawlRecipe;
 
 /**
  * @author hyeon
@@ -33,37 +34,38 @@ import study.java.project1.html.TagElement;
  */
 @RunWith(PowerMockRunner.class)
 @PowerMockRunnerDelegate(SpringJUnit4ClassRunner.class)
-@PrepareForTest(Jsoup.class)
+@PrepareForTest({ Jsoup.class })
 //왜인지 모르겠지만 PowerMock은 JavaConfig가 안 됨..
 @ContextConfiguration("classpath:study/java/project1/crawler/crawl_test.xml")
 public class CrawlerTest {
 	@Autowired
-	private NewsCrawler<List<TagElement>> contentsListExtractor;
+	private NewsCrawler<List<TagElement>> newsLinkCrawler;
 
-	private CrawlerContext mockContext;
 	@Before
 	public void init() throws Exception {
-		mockStatic(Jsoup.class);
 		
-		String mockUrlStr = "http://news.mock.myapp";
-		mockContext = new CrawlerContext();
-		mockContext.putParam(CrawlerContextProperty.SEED_URL, mockUrlStr);
-		mockContext.putParam(ContentsListProperty.CONTENT_LIST_SELECTOR, "ul li.edit a");
-		URL mockUrl = new URL(mockContext.getParam(CrawlerContextProperty.SEED_URL));
-		whenNew(URL.class).withAnyArguments().thenReturn(mockUrl);
-		
-		File expectedResource = new File(getClass().getResource("mock_news.html").toURI());
-		when(Jsoup.parse(expectedResource, "utf-8")).thenCallRealMethod(); //테스트 파일을 파싱하기 위해 real method 이용
-		
-		Document mockDoc = Jsoup.parse(expectedResource, "utf-8");
-		when(Jsoup.parse(mockUrl, ContentsListExtractor.DEFAULT_TIMEOUT_MILLIS)).thenReturn(mockDoc);
 	}
 
 	@Test
 	public void 게시판형_뉴스페이지의_seed에서_뉴스목록_뽑아오기() throws Exception {
-		//parse 메소드 내에서, Jsoup.parse는 mocking 될 것이다.
-		List<TagElement> elements = contentsListExtractor.parse(mockContext);
-		
+	  CrawlRecipe mockRecipe = mock(CrawlRecipe.class);
+    when(mockRecipe.getNewsLinkSelector()).thenReturn("ul li.edit a");
+    final String mockUrlStr = "http://news.mock.myapp";
+    
+    CrawlerContext mockContext = mock(CrawlerContext.class);
+    when(mockContext.getParam(CrawlerContextProperty.SEED_URL)).thenReturn(mockUrlStr);
+    when(mockContext.getParam(CrawlerContextProperty.RECIPE)).thenReturn(mockRecipe);
+
+    //parsing result mock
+    URL expectedDocUrl = getClass().getResource("mock_news.html");
+    File expectedResource = new File(expectedDocUrl.toURI());
+    Document mockDoc = Jsoup.parse(expectedResource, "utf-8");
+
+    mockStatic(Jsoup.class);
+    when(Jsoup.parse(any(URL.class), any(Integer.class))).thenReturn(mockDoc);    
+    
+	  //parse 메소드 내에서, Jsoup.parse는 mocking 될 것이다.
+    List<TagElement> elements = newsLinkCrawler.parse(mockContext);
 		assertEquals(3, elements.size());
 		assertEquals("first", elements.get(0).getAttribute("href"));
 		assertEquals("second", elements.get(1).getAttribute("href"));
