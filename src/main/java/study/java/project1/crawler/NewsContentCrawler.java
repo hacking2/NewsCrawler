34ab45c4a6b93e7a327294bafa909c3da3b128b1
volunt22r <volunt22r@naver.com>
2016-08-21 20:31:26 +0900

    설명이 필요한 부분 주석 보충

--- src/main/java/study/java/project1/crawler/NewsContentCrawler.java
+++ src/main/java/study/java/project1/crawler/NewsContentCrawler.java
@@ -12,6 +12,12 @@ import study.java.project1.byproduct.RawNews;
 import study.java.project1.model.CrawlRecipe;
 import study.java.project1.model.CrawlRecipe.IdSpot;
 
+/**
+ * 이 크롤러에 의해 정제되지 않은 기사 제목, 내용과 정제된 기사 ID를 뽑아온다.
+ * 정제의 의미는 텍스트만 깨끗하게 있지 않고, 이미지가 분리되지 않았으며, 기타 잡Tag들이 있는 경우를 말한다.
+ * @author hyeon
+ *
+ */
 public class NewsContentCrawler implements NewsCrawler<RawNews> {
   @Override
   public RawNews parse(CrawlerContext context) throws Exception {
@@ -19,9 +25,10 @@ public class NewsContentCrawler implements NewsCrawler<RawNews> {
     CrawlRecipe recipe = context.getParam(CrawlerContextProperty.RECIPE);
     Document doc = Jsoup.parse(new URL(url), NewsCrawler.DEFAULT_TIMEOUT_MILLIS);
     RawNews news = new RawNews();
+    //url에 기사 id가 심어져 있는 경우는 정규식으로 기사id를 추출하도록 한다.
     if (recipe.getIdSpot() == IdSpot.URL) {
       Matcher matcher = Pattern.compile(recipe.getIdSelector()).matcher(url);
-      if (matcher.groupCount() > 1) {
+      if (matcher.groupCount() > 1) { //id는 하나만 매칭되는 정규식이어야 한다.
         throw new Exception("Too many ID candidate in url");
       }
       if (matcher.find()) {
@@ -30,16 +37,17 @@ public class NewsContentCrawler implements NewsCrawler<RawNews> {
         throw new Exception("No such news ID in url");
       }
     } else {
+      //url에 id가 없는 특수한 경우, document에서 찾도록 한다.
       Elements idElement = doc.select(recipe.getIdSelector());
       String id = null;
       switch (recipe.getIdValueType()) {
-        case VALUE :
+        case VALUE :  //select되는 것이 input 등의 경우 value attribute에서 뽑아온다.
           id = idElement.val();
           break;
-        case TEXT :
+        case TEXT : //span 등 텍스트로 존재할 경우
           id = idElement.text();
           break;
-        case HTML :
+        case HTML : //HTML인 경우는 아마 없을 것 같지만 그래도 일단 가능성은 열어둔다.
           id = idElement.html();
           break;
         default : 
