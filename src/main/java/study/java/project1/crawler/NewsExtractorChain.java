209f8d1717a520af3a2857d2462505f2010a4930
volunt22r <volunt22r@naver.com>
2016-08-21 20:26:29 +0900

    크롤러가 긁어온 것을 News가 아닌 RawNews로 변경함(Normalize 후에 News가 되도록 하기 위해서임)

--- src/main/java/study/java/project1/crawler/NewsExtractorChain.java
+++ src/main/java/study/java/project1/crawler/NewsExtractorChain.java
@@ -8,24 +8,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import study.java.project1.byproduct.RawNews;
 import study.java.project1.html.TagElement;
-import study.java.project1.model.News;
 
 /**
  * 게시판형 뉴스페이지 크롤러로, 1차로 목록으로부터 뉴스기사 링크를 가져오며(contentsListExtractor), 
  * 이 링크를 다시 한 번 따라가서 실제 뉴스 제목, 내용을 긁어온다.(contentsExtractor) 
  * @author hyeon
  */
-public class NewsExtractorChain implements NewsCrawler<List<News>> {
+public class NewsExtractorChain implements NewsCrawler<List<RawNews>> {
   @Autowired
   private NewsCrawler<List<TagElement>> contentsListExtractor;
   
   @Autowired
-  private NewsCrawler<News> contentsExtractor;
+  private NewsCrawler<RawNews> contentsExtractor;
   
   @Override
-  public List<News> parse(CrawlerContext ctx) throws Exception {
-    List<News> news = new ArrayList<>();
+  public List<RawNews> parse(CrawlerContext ctx) throws Exception {
+    List<RawNews> news = new ArrayList<>();
     List<TagElement> links = contentsListExtractor.parse(ctx);
     links.forEach(linkElement -> {
       ctx.putParam(CrawlerContextProperty.SEED_URL, linkElement.getAttribute("abs:href"));
