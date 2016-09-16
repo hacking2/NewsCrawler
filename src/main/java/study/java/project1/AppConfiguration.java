599a79b5c0bf6ff04e28e922d11d4850e026eafa
volunt22r <volunt22r@naver.com>
2016-09-16 11:44:08 +0900

    이미지 서버 만들기 귀찮으므로 무료 이미지 서버 활용을 위해 설정함

--- src/main/java/study/java/project1/AppConfiguration.java
+++ src/main/java/study/java/project1/AppConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource(value = "classpath:properties/version.properties", encoding = "UTF-8")
+@PropertySource(value = "classpath:properties/external_api.properties", encoding = "UTF-8")
+@PropertySource(value = "file:/data/external_api.properties", ignoreResourceNotFound=true, encoding = "UTF-8")
 public class AppConfiguration {
 
 }
